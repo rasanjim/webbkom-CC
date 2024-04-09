8e2b186a0ab503f7e4c9f07e5d1ed7fc86ccfaa4
rasanjim <rasanen.jimmy.e@gmail.com>
2024-04-09 16:41:11 +0000

    database connection, hämta info från databasen CC

--- hotel-api/app.py
+++ hotel-api/app.py
@@ -1,7 +1,18 @@
+import os, psycopg
+from psycopg.rows import dict_row
 from flask import Flask, request
 from flask_cors import CORS 
+from dotenv import load_dotenv
+
+# pip install psycopg_binary och python-dotenv
+
+load_dotenv()
 
 PORT=8322
+
+db_url = os.environ.get("DB_URL")
+
+conn = psycopg.connect(db_url, autocommit=True, row_factory=dict_row)
 
 app = Flask(__name__)
 CORS(app) # Tillåt cross-origin requests
@@ -12,9 +23,19 @@ rooms = [
     { 'number': 303, 'type': "suite" }
 ]
 
+
 @app.route("/",)
 def info():
     return "<h1>Hotel API, endpoints /rooms, /bookings</h1>"
+
+
+@app.route("/test",)
+def dbtest():
+    with conn.cursor() as cur:
+        cur.execute("SELECT * from people")
+        rows = cur.fetchall()
+        return rows
+
 
 @app.route("/ip")
 def ip():
@@ -32,7 +53,7 @@ def rooms_endpoint():
     else:
         return rooms
 
-@app.route("/test/<int:id>", methods=['GET', 'PUT', 'PATCH', 'DELETE'])
+@app.route("/rooms/<int:id>", methods=['GET', 'PUT', 'PATCH', 'DELETE'])
 def one_room(id):
     if request.method == 'GET':
        return rooms [id]
