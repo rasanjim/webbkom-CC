a5eddd7206d29af21c30e3b383040d10e7a9d221
rasanjim <rasanen.jimmy.e@gmail.com>
2024-04-09 14:12:50 +0000

    rooms

--- hotel-api/app.py
+++ hotel-api/app.py
@@ -6,28 +6,48 @@ PORT=8322
 app = Flask(__name__)
 CORS(app) # Tillåt cross-origin requests
 
-@app.route("/")
-def hello():
-    return "<h1>Hello, Hotel-API!</h1>"
+rooms = [
+    { 'number': 101, 'type': "single" },
+    { 'number': 202, 'type': "double" },
+    { 'number': 303, 'type': "suite" }
+]
+
+@app.route("/",)
+def info():
+    return "<h1>Hotel API, endpoints /rooms, /bookings</h1>"
 
 @app.route("/ip")
 def ip():
     return { 'ip': request.remote_addr }
 
-@app.route("/test", methods=['GET', 'POST'])
-def test():
-    return {
-        'msg': "TESTING",
-        'method': request.method
-    }
-
-@app.route("/test/<int:id>", methods=['GET', 'PUT', 'DELETE'])
-def testId(id):
-    if request.method == 'GET':
+@app.route("/rooms", methods=['GET', 'POST'])
+def rooms_endpoint():
+    if request.method == 'POST':
+        request_body = request.get_json()
+        print(request_body)
+        rooms.append(request_body)
         return {
-        'msg': f"här får du id: {id}",
-        'method': request.method
-         }
+        'msg': f"Du har skapat ett nytt rum, id: {len(rooms)-1}!",
+        }
+    else:
+        return rooms
+
+@app.route("/test/<int:id>", methods=['GET', 'PUT', 'PATCH', 'DELETE'])
+def one_room(id):
+    if request.method == 'GET':
+       return rooms [id]
+    
+    if request.method == 'PUT' or request.method == 'PATCH':
+        return {
+            'msg': f"Du uppdaterar id: {id}",
+            'method': request.method
+        }
+    
+    if request.method == 'DELETE':
+        return {
+            'msg': f"Du har raderat {id}",
+            'method': request.method
+        }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT, debug=True, ssl_context=(
